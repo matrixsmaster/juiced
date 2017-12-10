6f547a0d6424fe4b22527d5047b2dfb5b3b35da9
matrixsmaster <matrixsmaster@yandex.ru>
2017-12-10 14:37:51 +0300

    Initial

---
+++ haha.h
@@ -0,0 +1,13 @@
+/*
+ * haha.h
+ *
+ *  Created on: Oct 2, 2017
+ *      Author: lisa
+ */
+
+#ifndef HAHA_H_
+#define HAHA_H_
+
+#define RANGED_FOR(TYPE,VAL,RANGE) for (TYPE::iterator VAL ## _ = RANGE.begin(); VAL ## _ != RANGE.end(); ++ VAL ## _) {
+
+#endif /* HAHA_H_ */
